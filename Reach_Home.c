f3370aecaf7db926d48c4f91980a67010bf62993
Bhavyateja04 <147074331+Bhavyateja04@users.noreply.github.com>
2023-10-25 08:51:30 +0530

    #include<stdio.h> int main() {     int x ,y ;     scanf("%d %d",&x,&y);     int a=x*1,b=x*5;     if(x<=a;y<=b)     {         printf("YES");     }     else     {         printf("NO");     }     }

---
+++ Reach_Home.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x ,y ;
+    scanf("%d %d",&x,&y);
+    int a=x*1,b=x*5;
+    if(x<=a;y<=b)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+    }
