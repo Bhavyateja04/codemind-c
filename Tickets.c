382d4bd81e43e41447e41ebda84e9184e843394c
Bhavyateja04 <147074331+Bhavyateja04@users.noreply.github.com>
2023-10-25 08:40:34 +0530

    #include<stdio.h> int main(){     int x;     scanf("%d",&x);     int y=4*x;     if (y<=1000)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Tickets.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int x;
+    scanf("%d",&x);
+    int y=4*x;
+    if (y<=1000)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
