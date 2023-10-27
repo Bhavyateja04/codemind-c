3c12b7fc19da833a42265dfd8dc14b7e12670f0e
Bhavyateja04 <147074331+Bhavyateja04@users.noreply.github.com>
2023-10-27 11:35:06 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     if (n%2!=0)     {         printf("weird");     }     else if (n%2==0,5<=n>=2)     {         printf("not weird");     }     else if (n%2==0,20<=n>=6)     {         printf("weird");     }     else {         printf("not weird");     } }

---
+++ Weird_or_not_Weird.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if (n%2!=0)
+    {
+        printf("weird");
+    }
+    else if (n%2==0,5<=n>=2)
+    {
+        printf("not weird");
+    }
+    else if (n%2==0,20<=n>=6)
+    {
+        printf("weird");
+    }
+    else {
+        printf("not weird");
+    }
+}
