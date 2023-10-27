5780e86078d485408800153c409b99d4bb92914a
Bhavyateja04 <147074331+Bhavyateja04@users.noreply.github.com>
2023-10-27 08:41:35 +0530

    #include<stdio.h> int main() {     char r,a,e,i,o,u,A,E,I,O,U;     scanf("%c",&r);     if (r == 'a'||r == 'e'||r == 'i'||r == 'o'||r == 'u'|| r == 'A'||r == 'E'||r == 'I'||r == 'O'||r == 'U')     {         printf("VOWEL");     }     else     {         printf("CONSONANT");     } }

---
+++ VOWEL_or_CONSONANT.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char r,a,e,i,o,u,A,E,I,O,U;
+    scanf("%c",&r);
+    if (r == 'a'||r == 'e'||r == 'i'||r == 'o'||r == 'u'|| r == 'A'||r == 'E'||r == 'I'||r == 'O'||r == 'U')
+    {
+        printf("VOWEL");
+    }
+    else 
+    {
+        printf("CONSONANT");
+    }
+}
