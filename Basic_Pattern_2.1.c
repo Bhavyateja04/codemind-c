9c6bc1549ae247f1771aca209301c180c363dd24
Bhavyateja04 <147074331+Bhavyateja04@users.noreply.github.com>
2023-11-04 11:41:30 +0530

    #include<stdio.h> int main() {     int j,n,i=0;     scanf("%d",&n);     for (i=1;i<=n;i=i+1){         for (j=0;j<i;j=j+1){             {                 printf("* ");             }         }printf(" ");     } }

---
+++ Basic_Pattern_2.1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int j,n,i=0;
+    scanf("%d",&n);
+    for (i=1;i<=n;i=i+1){
+        for (j=0;j<i;j=j+1){
+            {
+                printf("* ");
+            }
+        }printf("
+");
+    }
+}
